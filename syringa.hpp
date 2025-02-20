a8ad7f9f0e23d3a9fb73b653351b60bf820ecc18
Abdallh <correction62@gmail.com>
2025-02-20 03:51:44 +0200

    Instead of const.hpp file

---
+++ syringa.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// This file is supposed to contain some information related to the language, such as name, version, etc
+
+/*********************************************/
+#define LANGUAGE_NAME SYRINGA
+#define LANGUAGE_NAME_STR "SYRINGA"
+/*********************************************/
+#define language_name syringa
+#define language_name_str "syringa"
+/*********************************************/
+#define exten "sy"
